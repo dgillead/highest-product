0efab7f9a03dcec03813629f4dff0294f021d4b5
Darnell Gillead <dlgillead@gmail.com>
2017-06-28 10:15:13 -0500

    completed challenge

--- highest.js
+++ highest.js
@@ -1,50 +1,18 @@
-// class ProductFinder
-//   def initialize(array)
-//     @array = array.sort
-//   end
-//
-//   def find
-//     if all_negative?
-//       @array.last(3)
-//     else
-//       find_in_mixed_array
-//     end
-//   end
-//
-//   def find_in_mixed_array
-//     highest_positive_integer = @array.pop
-//     item1 = @array[0]
-//     item2 = @array[1]
-//     item3 = @array[@array.length - 1]
-//     item4 = @array[@array.length - 2]
-//
-//     if item1 * item2 > item3 * item4
-//       [item1, item2, highest_positive_integer]
-//     else
-//       [item4, item3, highest_positive_integer]
-//     end
-//   end
-//
-//   def all_negative?
-//     @array.all? { |i| i < 0 }
-//   end
-// end
-
 function productFinder(array) {
-  array = array.sort();
-  let product = 0;
+  array = array.sort(function(a,b) { return a - b });
+  let product = 1;
   if (allNegative(array)) {
-    console.log(array.slice((array.length - 4), (array.length - 1)));
+    product = findInAllNegativeArray(array);
   } else {
     product = findInMixedArray(array);
   }
-  console.log(product);
+  return product;
 }
 
 function allNegative(array) {
   let allNeg = true;
   for (let i = 0; i < array.length; i++) {
-    if (array[i] > 0) {
+    if (array[i] < 0) {
       allNeg = true;
     } else {
       return false;
@@ -67,4 +35,13 @@ function findInMixedArray(array) {
   }
 }
 
-productFinder([-2,-1,-3, 0, 1, 2]);
+function findInAllNegativeArray(array) {
+  let returnProduct = 1;
+  highestThree = array.slice(Math.max(array.length - 3, 0));
+  for (let i = 0; i < array.length; i++) {
+    returnProduct *= array[i];
+  }
+  return returnProduct;
+}
+
+productFinder([-10,-10,-9, 0, 2, 3, 4]);
